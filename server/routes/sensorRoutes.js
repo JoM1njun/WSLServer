c54938b2a57d3eba948c359ff8814010d44fb021
JoM1njun <brian2732@naver.com>
2026-05-15 19:39:17 +0900

    Refactor: Sensor API URL ReArrange

--- server/routes/sensorRoutes.js
+++ server/routes/sensorRoutes.js
@@ -10,10 +10,10 @@ const router = express.Router();
 // 센서 데이터 저장 + 최신 센서 데이터 갱신 + Alert 생성
 router.post("/", insertSensorData);
 
-// 특정 작업자 + 헬멧의 최신 센서 데이터 조회
-router.get("/:workerId/:helmetId", getLatestSensorData);
-
 // 특정 작업자의 센서 기록 조회
 router.get("/workers/:workerId", getWorkerSensorData);
+
+// 특정 작업자 + 헬멧의 최신 센서 데이터 조회
+router.get("/:workerId/:helmetId", getLatestSensorData);
 
 export default router;
